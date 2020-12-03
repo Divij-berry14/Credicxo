a4678a7776f187777ceec17d7a3ebee8ff59fc8c
Divij <divij.berry1999@gmail.com>
2020-12-03 23:09:15 +0530

    user serializers

---
+++ Api/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class StudentSerializer(serializers.ModelSerializer):
+    """
+    The details for the users in the group Student
+    """
+    class Meta:
+        model = User
+        fields = ['id','first_name','last_name','email',]
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for complete details of all the users in the database
+    """
+    class Meta:
+        model = User
+        fields = '__all__'
--- Credicxo/settings.py
+++ Credicxo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'Api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'Credicxo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Credixco',
+        'USER': 'postgres',
+        'PASSWORD': '789456123',
+        'HOST': 'localhost'
     }
 }
 
