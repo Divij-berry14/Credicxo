fb8a1f2657a1e8b568574de754df1f05b13d4a09
Divij <divij.berry1999@gmail.com>
2020-12-03 23:54:39 +0530

    changes in settings.py file

--- Api/serializers.py
+++ Api/serializers.py
@@ -9,7 +9,7 @@ class StudentSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = User
-        fields = ['id','first_name','last_name','email',]
+        fields = ['id', 'first_name', 'last_name', 'email',]
 
 class UserSerializer(serializers.ModelSerializer):
     """
--- Credicxo/settings.py
+++ Credicxo/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!8y&oqt2uwupgbffb71mjh)t_3#!krc&vd#d4n8(z%7z-$nazs'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Credixco',
         'USER': 'postgres',
-        'PASSWORD': '789456123',
+        'PASSWORD': os.getenv("DB_PASS"),
         'HOST': 'localhost'
     }
 }
@@ -124,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
