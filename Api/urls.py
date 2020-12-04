288dacbf87de68eff64a6cc10632ae73197f540d
Divij <divij.berry1999@gmail.com>
2020-12-04 15:27:42 +0530

    Api urls

--- Api/urls.py
+++ Api/urls.py
@@ -1,5 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-
+    path("auth/", include('djoser.urls')),
+    path("auth/", include('djoser.urls.jwt'))
 ]
+
+
+
+
+"""
+Some important djoser urls
+To create a user:                           /users/
+To update the authenticated user:           /users/me/
+To set a new password for existing users:   /users/set_password/
+To receive email with reset password link:  /users/reset_password/
+To Login/generate JWT:                      /jwt/create/
+To refresh the JWT:                         /jwt/refresh/
+To Verify the JWT:                          /jwt/verify/
+"""
